51ba24d6fe48772096f97b265f1bf64a103cb2a9
Thomas Duvinage <43036801+ThomasDuvinage@users.noreply.github.com>
2025-02-03 15:09:33 +0900

    [widgets] add show option to transform

--- src/widgets/Transform.h
+++ src/widgets/Transform.h
@@ -11,9 +11,23 @@ struct TransformWidget : public TransformBase<ControlAxis::ALL>
 
   void draw3D() override
   {
+    if(!show_)
+    {
+      return;
+    }
+    
     TransformBase::draw3D();
     mclient_.draw_frame(marker_.pose());
   }
+
+  void draw2D() override
+  {
+    ImGui::Checkbox(label(fmt::format("Show {}", id.name)).c_str(), &show_);
+  }
+
+  private:
+    bool show_ = true;
 };
 
 } // namespace mc_mujoco
+
