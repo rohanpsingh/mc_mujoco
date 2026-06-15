8ca3efe89478c96abf74bd30675ccdc424bcf178
Arnaud TANGUY <arn.tanguy@gmail.com>
2026-06-15 13:59:45 +0200

    nix: use boost-free dependencies (mc_rtc-imgui) and cli11

--- flake.nix
+++ flake.nix
@@ -25,9 +25,22 @@
 
             # Override dependencies with flakoboros module
             flakoboros = {
-              overrideAttrs.mc-mujoco = {
-                src = lib.cleanSource ./.;
-              };
+              overrideAttrs.mc-rtc-imgui =
+                { pkgs-final, ... }:
+                {
+                  src = pkgs-final.fetchFromGitHub {
+                    owner = "mc-rtc";
+                    repo = "mc_rtc-imgui";
+                    rev = "6ac125c00ca5e18c7da80e0049dc5697f4f36a23";
+                    hash = "sha256-J9uQpfn08Yv5ROxQmxHYwiVRdvZsVOp2klZmgnw90Bg=";
+                  };
+                };
+              overrideAttrs.mc-mujoco =
+                { drv-prev, pkgs-final, ... }:
+                {
+                  src = lib.cleanSource ./.;
+                  nativeBuildInputs = drv-prev.nativeBuildInputs ++ [ pkgs-final.cli11 ];
+                };
             };
           }
         ];
